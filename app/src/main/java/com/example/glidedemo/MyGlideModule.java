39730e88b6af70610b2d10ea943c6be91411d283
zhangzhongjie <zhangzhongjie@zongheng.com>
2019-03-21 14:03:59 +0800

    develop

--- app/src/main/java/com/example/glidedemo/MyGlideModule.java
+++ app/src/main/java/com/example/glidedemo/MyGlideModule.java
@@ -20,6 +20,8 @@ public class MyGlideModule extends AppGlideModule {
     @Override
     public void applyOptions(@NonNull Context context, @NonNull GlideBuilder builder) {
         super.applyOptions(context, builder);
+        //master
         builder.setResizeExecutor()
+
     }
 }
